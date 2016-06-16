84876e7b17f712c4cd5f924b199c3bafb6de2592
variani <johndoe@example.com>
2016-06-16 17:22:34 +0200

    added bench

---
+++ inst/benchmarks/bench-06-eigenPower-ncomp.R
@@ -0,0 +1,38 @@
+library(plyr)
+library(ggplot2)
+
+library(microbenchmark)
+
+hilbert <- function(n) { 
+  i <- 1:n
+  1 / outer(i - 1, i, "+") 
+}
+
+
+nseq <- seq(100, 1200, length = 5)
+df <- ldply(nseq, function(n) {
+  n <- ceiling(n)
+  v <- runif(n)
+  M <- hilbert(n)
+  
+  cat(" * n:", n, "\n")
+  
+  out <- microbenchmark(
+    eigen(M),
+    eigenPower(M), 
+    eigenPower(M, ncomp = 5),
+    eigenPowerRcppEigen(M), 
+    times = 10)
+  
+  df <- subset(as.data.frame(summary(out)), select = c("expr", "median"))
+  df$n <- n
+  
+  return(df)
+})  
+
+p1 <- ggplot(df, aes(n, median, color = expr)) + geom_point() + geom_line()
+p1
+
+p2 <- ggplot(subset(df, n > 500), aes(n, median, color = expr)) + geom_point() + geom_line()
+p2
+
